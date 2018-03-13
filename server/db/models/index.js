7b820c26837d158380c50a88527621dcee9ea086
Melissa Bellah <melissabellah@gmail.com>
2018-03-13 09:11:12 -0400

    associations

---
+++ server/db/models/index.js
@@ -0,0 +1,9 @@
+'use strict';
+const db = require('../index.js');
+const JobPosting = require('./job-posting.js');
+const Document = require('./document.js');
+const Reference = require('./reference');
+
+Document.belongsTo(JobPosting);
+Reference.belongsTo(JobPosting);
+JobPosting.hasMany(Reference);
