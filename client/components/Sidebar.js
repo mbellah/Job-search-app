3449047fa71a642082c7eb847f6dc0a2f9d81817
Melissa Bellah <melissabellah@gmail.com>
2018-03-15 14:48:58 -0400

    fixed error

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import JobTitleList from './JobTitleList';
 
 export default function Sidebar() {
   return (
     <sidebar>
       <div>
         <h3>Job Postings</h3>
-        <JobTitleList />
       </div>
     </sidebar>
   )
--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Main } from './components';
+import Main from './components/Main.js';
 
 ReactDOM.render(
   <Router>
